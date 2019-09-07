eb073c8116a2edb22b39c09b907d492fa78705b3
Luis Felipe <felipe141099@gmial.com>
2019-09-06 19:54:26 -0500

    Interfaz v1
    Text

---
+++ interfaz.py
@@ -0,0 +1,30 @@
+import sys
+from PyQt5 import uic, QtWidgets
+from AnalizadorLexico import Analizador
+
+qtCreatorFile = "interfazQT.ui" # Nombre del archivo aquí.
+
+Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        QtWidgets.QMainWindow.__init__(self)
+        Ui_MainWindow.__init__(self)
+        self.setupUi(self)
+        self.btnAnalizar.clicked.connect(self.AnalisisLexico)
+
+    def AnalisisLexico(self):
+        codigo = self.txtCodigo.toPlainText()
+        print(codigo)
+        analisisLexico = Analizador(codigo)
+
+
+        analisisLexico.analizar()
+
+        
+
+if __name__ == "__main__":
+    app =  QtWidgets.QApplication(sys.argv)
+    window = MyApp()
+    window.show()
+    sys.exit(app.exec_())
